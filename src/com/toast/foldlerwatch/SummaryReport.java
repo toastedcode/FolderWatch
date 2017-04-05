4767a5ebe0f2867f26ea5975959bab3d5503a756
toastedcode <jasontost@gmail.com>
2017-04-05 06:50:40 -0400

    Started summary report.

---
+++ src/com/toast/foldlerwatch/SummaryReport.java
@@ -0,0 +1,31 @@
+package com.toast.foldlerwatch;
+
+public class SummaryReport
+{
+   
+   public SummaryReport()
+   {
+   }
+   
+   void addReport(OasisReport report)
+   {
+      
+   }
+   
+   public String toText()
+   {
+      String text = "";
+      
+      return (text);
+   }
+   
+   public String toHtml()
+   {
+      String html = "";
+      
+      return (html);
+   }
+   
+   private Map<String, >
+   
+}
