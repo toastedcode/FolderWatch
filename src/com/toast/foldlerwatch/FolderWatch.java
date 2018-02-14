fc37a154beca36fa9fd4d72016132b511f617cc1
toastedcode <jasontost@gmail.com>
2018-02-14 10:08:06 -0500

    Added debounce() to handle multiple ON_CREATE events for a single file.

--- src/com/toast/foldlerwatch/FolderWatch.java
+++ src/com/toast/foldlerwatch/FolderWatch.java
@@ -225,8 +225,6 @@ public class FolderWatch
    
    static private void watchLoop()
    {
-      long lastEventTime = 0;
-      
       while (true)
       {
 
@@ -241,35 +239,31 @@ public class FolderWatch
              return;
          }
          
-         long eventTime = System.currentTimeMillis();
-         if ((eventTime - lastEventTime) > 100)
+         for (WatchEvent<?> event: key.pollEvents())
          {
-            for (WatchEvent<?> event: key.pollEvents())
-            {
-                WatchEvent.Kind<?> kind = event.kind();
-   
-                // This key is registered only for ENTRY_CREATE events, but an OVERFLOW event can
-                // occur regardless if events are lost or discarded.
-                if (kind == StandardWatchEventKinds.OVERFLOW)
-                {
-                    continue;
-                }
-                
-                // Extract the watched folder.
-                Path watchedPath = (Path)key.watchable();
-   
-                // The filename is the context of the event.
-                @SuppressWarnings("unchecked")
-                WatchEvent<Path> ev = (WatchEvent<Path>)event;
-                String filename = ev.context().toString();
-                
-                Path fullPath = Paths.get(watchedPath.toString() + "\\" + filename);
-                
-                if (isWatchTime())
-                {
-                   onNewFileDetected(fullPath);
-                }
-            }
+             WatchEvent.Kind<?> kind = event.kind();
+
+             // This key is registered only for ENTRY_CREATE events, but an OVERFLOW event can
+             // occur regardless if events are lost or discarded.
+             if (kind == StandardWatchEventKinds.OVERFLOW)
+             {
+                 continue;
+             }
+             
+             // Extract the watched folder.
+             Path watchedPath = (Path)key.watchable();
+
+             // The filename is the context of the event.
+             @SuppressWarnings("unchecked")
+             WatchEvent<Path> ev = (WatchEvent<Path>)event;
+             String filename = ev.context().toString();
+             
+             Path fullPath = Paths.get(watchedPath.toString() + "\\" + filename);
+             
+             if (isWatchTime() && debounce(filename, System.currentTimeMillis()))
+             {
+                onNewFileDetected(fullPath);
+             }
          }
 
          // Reset the key -- this step is critical if you want to receive further watch events.  
@@ -279,8 +273,6 @@ public class FolderWatch
          {
              break;
          }
-         
-         lastEventTime = eventTime;
       }      
    }
    
@@ -607,6 +599,19 @@ public class FolderWatch
       return (folders);
    }
    
+   static boolean debounce(String filename, long eventTime)
+   {
+      final long DEBOUNCE_TIME = 100;  // milliseconds
+      
+      boolean shouldProcess = ((!filename.equals(lastFilename)) ||
+                               ((eventTime - lastEventTime) > DEBOUNCE_TIME));
+
+      lastEventTime = eventTime;
+      lastFilename = filename;
+      
+      return (shouldProcess);
+   }
+   
    static boolean isWatchTime()
    {
       boolean isTime = true;
@@ -717,5 +722,9 @@ public class FolderWatch
    static private Properties properties;
    
    static WatchService watchService;
+   
+   static String lastFilename;
+   
+   static long lastEventTime;
    
 }
